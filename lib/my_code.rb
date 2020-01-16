1536895c8d2bda077dd7a585f633504c141c44a3
Greg Wagner <gpwagner@gmail.com>
2020-01-16 15:01:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,13 +10,6 @@ def map(array)
 end
 
 def reduce(array, starting_point = 0)
-  if starting_point
-    total = starting_point
-    i = 0
-  else
-    total = array[0]
-    i = 1
-  end
   while i < array.length do
     total = yield(total, array[i])
     i += 1
