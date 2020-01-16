c97f39d176ea51ec1169fd26a300b70c06f1dbc4
Greg Wagner <gpwagner@gmail.com>
2020-01-16 14:57:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,13 @@ def map(array)
   end
   return_array
 end
+
+def reduce_to_total(array, starting_point = 0)
+  i=0
+total = starting_point
+  while i < array.length do
+    total += yield(total, array[i])
+    i += 1
+  end
+total
+end
