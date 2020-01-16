150786019dcc280743b8d01efc3400e36636827e
Greg Wagner <gpwagner@gmail.com>
2020-01-16 14:58:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def reduce(array, starting_point = 0)
   i=0
 total = starting_point
   while i < array.length do
-    total += yield(total, array[i])
+    total = yield(total, array[i])
     i += 1
   end
 total
